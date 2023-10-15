b34a88f4ce1012cd11afc8eb5dd77f8204e37a96
Jim Campbell <jimcampbell355@gmail.com>
2023-10-14 20:01:01 -0400

    initial commit

---
+++ JsonParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeParser
+{
+    public class JsonParser
+    {
+        public static string ParseFileForJson(string path)
+        {
+            Parser parser = new(path);
+            parser.ReplaceEscapeCharacters();
+            return parser.GetFormattedContent();
+        }
+    }
+}
---
+++ Parser.cs
@@ -0,0 +1,33 @@
+﻿namespace CodeParser
+{
+    public class Parser
+    {
+        private readonly string _errorMessage = "No content in file, check file path.";
+        private string _content;
+        public Parser(string pathToFile)
+        {
+            Stream stream = File.OpenRead(pathToFile);
+            StreamReader sr = new StreamReader(stream);
+            string _content = sr.ReadToEnd();
+        }
+        public void ReplaceEscapeCharacters()
+        {
+            validateContent();
+            _content.Replace("\n", "");
+            _content.Replace("\t", "");
+        }
+        public string GetFormattedContent()
+        {
+            validateContent();
+            return _content;
+        }
+
+        private void validateContent()
+        {
+            if (string.IsNullOrEmpty(_content))
+            {
+                throw new Exception(_errorMessage);
+            }
+        }
+    }
+}
